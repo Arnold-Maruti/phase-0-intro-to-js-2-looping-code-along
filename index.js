97aface2377c1d2aaebe176b8d9253c0aa671346
Arnold <Arnold>
2025-03-13 00:10:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(number){
+    let count=number
+    while(count>=0){
+        console.log(count);
+        count--;
+    }
+}
+
+function writeCards(array,event){
+    let message=[]
+    for(let i=0;i<array.length;i++){
+           message.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+          }
+    return message;
+}
